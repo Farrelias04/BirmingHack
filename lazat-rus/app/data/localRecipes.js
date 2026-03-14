e503b0d3a86780ff9ed3f0a390f951a3cc52dce4
nawal <nawalforstudy@gmail.com>
2026-03-14 19:10:47 +0000

    add local recipe

---
+++ lazat-rus/app/data/localRecipes.js
@@ -0,0 +1,24 @@
+const localRecipes = [
+  {
+    id: "local-1",
+    idMeal: "local-1",
+    strMeal: "Nasi Lemak",
+    strCategory: "Malaysian classic",
+    strMealThumb: "/nasi-lemak.jpg",
+    strInstructions:
+      "Cook rice with coconut milk and a pinch of salt.\nFry anchovies until crispy and set aside.\nHard boil the egg and slice in half.\nServe rice with egg, anchovies, chili sambal and cucumber slices.",
+    ingredients: [
+      "2 cups Rice",
+      "1 cup Coconut Milk",
+      "2 Eggs",
+      "100g Anchovies",
+      "3 tbsp Chili Sambal",
+      "1 Cucumber",
+      "Salt to taste",
+    ],
+    keywords: ["rice", "coconut milk", "egg", "anchovies", "chili", "cucumber"],
+    isLocal: true,
+  },
+];
+
+export default localRecipes;
